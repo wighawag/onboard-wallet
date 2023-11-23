b3d888ef1496a0feac874ae7a610e5827f28b88b
wighawag <wighawag@gmail.com>
2023-11-23 12:48:28 +0000

    sri

--- iframe/vite.config.js
+++ iframe/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 // import { viteSingleFile } from "vite-plugin-singlefile";
+import sri from "@small-tech/vite-plugin-sri";
 
 export default defineConfig({
   //   plugins: [viteSingleFile()],
+  plugins: [sri()],
   build: {
     outDir: "../public/iframe",
     emptyOutDir: true,
